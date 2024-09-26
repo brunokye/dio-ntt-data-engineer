41c4d2273171ba4a9810063e53eaedfc58c1c6e8
brunokye <contatobrunokye@gmail.com>
2024-09-26 18:36:31 -0300

    feat: adiciona funções de usuário, conta corrente e login

--- python/sistema-bancario/app.py
+++ python/sistema-bancario/app.py
@@ -1,104 +1,195 @@
 from datetime import datetime
 
+users = {}
+accounts = []
+current_user = None
+account_number = 1
 
-def mostrar_menu():
+
+def show_menu():
     menu = """
     Use apenas números durante toda interação com o sistema.
-    [1] Depositar
-    [2] Sacar
-    [3] Extrato
+    [1] Criar Usuário
+    [2] Login
+    [3] Criar Conta Corrente
+    [4] Depositar
+    [5] Sacar
+    [6] Extrato
     [0] Sair
 
     """
     return input(menu)
 
 
-def deposit(balance, extract, number_of_transactions):
+def create_user():
+    nome = input("\nInforme o nome do usuário: ")
+    cpf = input("Informe o CPF do usuário (apenas números): ")
+
+    if cpf in users:
+        print("Operação falhou! Já existe um usuário cadastrado com esse CPF.")
+        return
+
+    endereco = input(
+        "Informe o endereço (logradouro, número - "
+        "bairro - cidade/sigla estado): "
+    )
+
+    users[cpf] = {
+        "nome": nome,
+        "endereco": endereco,
+    }
+
+    print(f"Usuário {nome} criado com sucesso!")
+
+
+def login_user():
+    global current_user
+    cpf = input("\nInforme o CPF do usuário para login: ")
+    if cpf in users:
+        current_user = cpf
+        print(f"Usuário {users[cpf]['nome']} logado com sucesso!")
+    else:
+        print("Operação falhou! Usuário não encontrado.")
+
+
+def create_checking_account():
+    global current_user, account_number
+
+    if current_user is None:
+        print(
+            "\nNenhum usuário logado. Por favor, crie ou selecione um usuário."
+        )
+        return
+
+    if (
+        current_user in users
+        and "contas" in users[current_user]
+        and len(users[current_user]["contas"]) >= 3
+    ):
+        print("\nUsuário já possui o número máximo de contas!")
+        return
+
+    account = {
+        "agencia": "0001",
+        "numero": account_number,
+        "usuario": current_user,
+        "balance": 0,
+        "extract": "",
+        "number_of_transactions": 0,
+        "number_of_withdrawals": 0,
+        "LIMIT_OF_TRANSACTIONS": 10,
+        "LIMIT_OF_WITHDRAWALS": 3,
+        "limit": 500,
+    }
+
+    if current_user in users:
+        if "contas" not in users[current_user]:
+            users[current_user]["contas"] = []
+        users[current_user]["contas"].append(account)
+
+    accounts.append(account)
+    print(
+        "\nConta corrente criada com sucesso! "
+        f"Número da conta: {account_number}"
+    )
+    account_number += 1
+
+
+def deposit(account):
     value = float(input("Informe o valor do depósito: "))
 
     if value > 0:
-        balance += value
+        account["balance"] += value
         timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        extract += f"Depósito: R$ {value:.2f} - {timestamp} \n"
-        number_of_transactions += 1
+        account["extract"] += f"Depósito: R$ {value:.2f} - {timestamp} \n"
+        account["number_of_transactions"] += 1
         print("Depósito realizado com sucesso!")
     else:
         print("Operação falhou! O valor informado é inválido.")
 
-    return balance, extract, number_of_transactions
 
-
-def withdraw(
-    balance,
-    extract,
-    number_of_transactions,
-    number_of_withdrawals,
-    limit,
-    LIMIT_OF_WITHDRAWALS,
-):
+def withdraw(account):
     value = float(input("Informe o valor do saque: "))
 
-    if value > balance:
+    if value > account["balance"]:
         print("Operação falhou! Você não tem saldo suficiente.")
     elif value < 0:
         print("Operação falhou! O valor informado é inválido.")
-    elif value > limit:
+    elif value > account["limit"]:
         print("Operação falhou! O valor do saque excede o limite diário.")
-    elif number_of_withdrawals >= LIMIT_OF_WITHDRAWALS:
+    elif account["number_of_withdrawals"] >= account["LIMIT_OF_WITHDRAWALS"]:
         print("Operação falhou! Número máximo de saques diários atingido.")
     else:
-        balance -= value
+        account["balance"] -= value
         timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        extract += f"Saque: R$ {value:.2f} - {timestamp} \n"
-        number_of_transactions += 1
-        number_of_withdrawals += 1
+        account["extract"] += f"Saque: R$ {value:.2f} - {timestamp} \n"
+        account["number_of_transactions"] += 1
+        account["number_of_withdrawals"] += 1
         print("Saque realizado com sucesso!")
 
-    return balance, extract, number_of_transactions, number_of_withdrawals
 
-
-def show_extract(balance, extract):
+def show_extract(account):
     print("\n================ EXTRATO ================")
-    print("Não foram realizadas movimentações." if not extract else extract)
-    print(f"\nSaldo: R$ {balance:.2f}")
+    print(
+        "Não foram realizadas movimentações."
+        if not account["extract"]
+        else account["extract"]
+    )
+    print(f"\nSaldo: R$ {account['balance']:.2f}")
     print("==========================================")
 
 
+def select_account():
+    global current_user
+    if current_user is None:
+        print("\nNenhum usuário logado.")
+        return None
+
+    user_accounts = users[current_user].get("contas", [])
+    if not user_accounts:
+        print("Nenhuma conta encontrada para o usuário.")
+        return None
+
+    print("\nSelecione a conta:")
+    for i, acc in enumerate(user_accounts):
+        print(f"[{i + 1}] Agência: {acc['agencia']}, Conta: {acc['numero']}")
+
+    selected_index = int(input("Digite o número da conta desejada: ")) - 1
+    if 0 <= selected_index < len(user_accounts):
+        return user_accounts[selected_index]
+    else:
+        print("Opção inválida.")
+        return None
+
+
 def run_atm():
-    balance = 0
-    limit = 500
-    extract = ""
-    number_of_transactions = 0
-    number_of_withdrawals = 0
-    LIMIT_OF_TRANSACTIONS = 10
-    LIMIT_OF_WITHDRAWALS = 3
-
+    global current_user
     while True:
-        if number_of_transactions >= LIMIT_OF_TRANSACTIONS:
-            print("Número máximo de transações diárias atingido.")
-            break
-
-        option = mostrar_menu()
+        option = show_menu()
 
         if option == "1":
-            balance, extract, number_of_transactions = deposit(
-                balance, extract, number_of_transactions
-            )
+            create_user()
 
         elif option == "2":
-            balance, extract, number_of_transactions, number_of_withdrawals = (
-                withdraw(
-                    balance,
-                    extract,
-                    number_of_transactions,
-                    number_of_withdrawals,
-                    limit,
-                    LIMIT_OF_WITHDRAWALS,
-                )
-            )
+            login_user()
 
         elif option == "3":
-            show_extract(balance, extract)
+            create_checking_account()
+
+        elif option == "4":
+            account = select_account()
+            if account is not None:
+                deposit(account)
+
+        elif option == "5":
+            account = select_account()
+            if account is not None:
+                withdraw(account)
+
+        elif option == "6":
+            account = select_account()
+            if account is not None:
+                show_extract(account)
 
         elif option == "0":
             break
